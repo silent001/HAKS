2b23a75a00e79c5e45247db92af2098e06f70a76
silent001 <martinnr@live.co.za>
2018-05-25 19:33:30 +0200

    Update install.sh

--- dependencies/dev/teamviewer/install.sh
+++ dependencies/dev/teamviewer/install.sh
@@ -3,5 +3,7 @@ sudo apt-get update && sudo apt-get -y upgrade
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
 sudo dpkg -i teamviewer-host_armhf.deb
 sudo apt-get -f install --yes
-read -p "Enter Your Teamviewer LAN Password: "  password
-sudo teamviewer setup && sudo teamviewer passwd $password && sudo rm teamviewer-host_armhf.deb*
+read -s -p "Create Your Teamviewer LAN Password: "  pswd
+read -s -p "Re-Enter Your Teamviewer LAN Password: "  pswd1
+#Here will go a if function to test if passwords match
+sudo teamviewer setup && sudo teamviewer passwd $pswd && sudo rm teamviewer-host_armhf.deb*

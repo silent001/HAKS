e9e40e9384c80673fdc38be0fa2ed6a09e442d92
silent001 <martinnr@live.co.za>
2018-05-25 19:10:34 +0200

    Update install.sh

--- dependencies/dev/teamviewer/install.sh
+++ dependencies/dev/teamviewer/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get -y upgrade
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
 sudo dpkg -i teamviewer-host_armhf.deb
 sudo apt-get -f install
